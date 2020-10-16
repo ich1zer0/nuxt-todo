05cfed9f935be991286e5882ed93677c5aba5616
徳永貴文 <proton.ich1@protonmail.com>
2020-10-16 09:52:51 +0900

    Add TodoInput.spec.js

---
+++ __tests__/components/TodoInput.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount } from '@vue/test-utils'
+import Component from '@/components/TodoInput.vue'
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = shallowMount(Component)
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
+describe('TodoInput', () => {
+  test('Vueのインスタンスである', () => {
+    expect(wrapper.isVueInstance).toBeTruthy()
+  })
+  test('最初はデータが空っぽである', () => {
+    expect(wrapper.vm.title).toMatch('')
+    expect(wrapper.vm.text).toMatch('')
+  })
+  test('emitデモ', async () => {
+    // data に値を入力
+    wrapper.vm.title = 'タスクのタイトル'
+    wrapper.vm.text = 'タスクの詳細'
+
+    // emit
+    wrapper.vm.$emit('submit', {
+      title: wrapper.vm.title,
+      text: wrapper.vm.text,
+    })
+
+    // emitが処理されるまで待つ
+    await wrapper.vm.$nextTick()
+
+    // イベントがemitされた
+    expect(wrapper.emitted('submit')).toBeTruthy()
+    // 一個のイベントがemitされた
+    expect(wrapper.emitted('submit').length).toBe(1)
+    // payloadされたデータ
+    expect(wrapper.emitted('submit')[0][0].title).toMatch('タスクのタイトル')
+  })
+})
